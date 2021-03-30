2c101e295469a62b5931f9c5d4d51c679cc699e8
Devashrutha <61559101+Devashrutha@users.noreply.github.com>
2021-03-31 01:56:22 +0530

    Add files via upload

---
+++ composition.py
@@ -0,0 +1,20 @@
+#Composition is a subset of association
+#One class is dependent on the other
+#That is one cannot exist without the other
+
+class salary:
+    def __init__(self,pay,bonus):
+        self.pay=pay
+        self.bonus=bonus
+    def annual_salary(self):
+        return self.pay*12+self.bonus
+class employee:
+    def __init__(self,name,age,pay,bonus):
+        self.name=name
+        self.age=age
+        self.Salary=salary(pay,bonus) #Initializing Class variables of salary to Class employee
+    def total_salary(self):
+        return self.Salary.annual_salary()
+
+emp=employee("Dev",22,100000,100000)
+print(emp.total_salary())
